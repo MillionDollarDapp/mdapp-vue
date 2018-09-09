b24127fea08e21d766eed32cbaef4b8516a3eba1
haggins <patrick@mindf.org>
2018-09-09 11:15:17 +0200

    Init contracts only if they're deployed to current network

--- src/util/getSaleContract.js
+++ src/util/getSaleContract.js
@@ -6,6 +6,11 @@ import web3Manager from './web3Manager'
 const getSaleContract = () => {
   let web3 = web3Manager.getInstance()
   let web3Watcher = web3Manager.getInstance(true)
+  if (!contractArtifacts.networks[store.state.web3.networkId]) {
+    // Contract not deployed on this network.
+    return [null, null]
+  }
+
   return [
     web3 ? new web3.eth.Contract(contractArtifacts.abi, contractArtifacts.networks[store.state.web3.networkId].address) : null,
     web3Watcher ? new web3Watcher.eth.Contract(contractArtifacts.abi, contractArtifacts.networks[store.state.web3.networkId].address) : null
--- src/util/getTokenContract.js
+++ src/util/getTokenContract.js
@@ -4,6 +4,11 @@ import web3Manager from './web3Manager'
 
 const getTokenContract = () => {
   let web3 = web3Manager.getInstance()
+  if (!contractArtifacts.networks[store.state.web3.networkId]) {
+    // Contract not deployed on this network.
+    return null
+  }
+
   return web3 ? new web3.eth.Contract(contractArtifacts.abi, contractArtifacts.networks[store.state.web3.networkId].address) : null
 }
 export default getTokenContract
